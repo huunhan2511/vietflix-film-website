74842c84dcecba0d887226f1f7692a4ff68dbbcc
Haotk <mrtkh1220@gmail.com>
2022-04-28 20:39:27 +0700

    [BE] GraphQL with fakeData

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
-import express from "express";
-//import apolloServer from 'apollo-server-express';
-import connectDB from './mongo/connectDB.js';
-connectDB.connectDB();
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import typeDefs from './schemas/schemas.js'
+import resolvers from './resolvers/resolvers.js'
 
 const app = express()
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start()
+server.applyMiddleware({app})
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Chạy trên cổng [" + PORT + "]"));
+app.listen(PORT, () => console.log("Running at: localhost:" + PORT + ""+ server.graphqlPath));
 
+
+// server.listen().then(({ url }) => {
+//   console.log(`🚀  Server ready at ${url}`);
+// });
