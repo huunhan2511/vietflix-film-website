8362adf482df92522c4554e29d1b4046e30a530f
phuongnam2820 <phuongnam2820@gmail.com>
2022-05-07 20:12:29 +0700

    [BE] UI LOGIN ADMIN

--- client/src/view/Admin/LoginAdmin/index.jsx
+++ client/src/view/Admin/LoginAdmin/index.jsx
@@ -10,6 +10,22 @@ export default function LoginAdmin() {
     navigate('/admin',{replace:true});
 }
   return (
-    <button className='border-2 p-4' onClick={handleLogin}>LoginAdmin</button>
+    <div className='flex justify-center'>
+        <div className='border-2 rounded-md p-6 w-1/3 h-auto absolute top-1/3'>
+          <div className='text-3xl font-bold text-center'> Đăng nhập</div>
+          <input 
+          type="email" 
+          placeholder='Nhập email'
+          className='w-full p-4 mt-4 outline-none rounded-md text-lg font-white'></input>
+
+          <input 
+          type="password" 
+          placeholder='Nhập mật khẩu'
+          className='w-full p-4 mt-4 outline-none rounded-md text-lg'></input>
+
+          <button className='btnLogIn bg-red-600' onClick={handleLogin}>Đăng nhập</button>
+        </div>
+    </div>
+
   )
 }
