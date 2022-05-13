7ec0115a711da9de2fffd7d27c95802bb2b08aa6
khanhtoan20 <57028524+khanhtoan20@users.noreply.github.com>
2022-05-13 20:46:37 +0700

    [BE] Authorization

--- server/jwt/jwtToken.js
+++ server/jwt/jwtToken.js
@@ -3,7 +3,8 @@ import { ApolloError} from "apollo-server-express";
 import Admin from '../models/admin.js'
 import dotenv from 'dotenv'
 dotenv.config();
-
+const SECRET_KEY = process.env.SECRET_KEY || "Vietflix";
+const ACCESS_DENIED = ['Từ chối truy cập','ACCESS DENIED']
 const token = {
     createToken: admin => {
         const tokenLife = process.env.tokenLife || 3600*6
@@ -37,6 +38,28 @@ const token = {
                 }
             } catch (error) {
                 throw new ApolloError('Từ chối truy cập', 'ACCESS DENIED')
+            }
+        }
+    },
+    // UNDER CONSTRUCED FOR AUTHORIZATION
+    verify: async req => {
+        let authorization = req.headers.authorization
+        if (!authorization) {
+            return new ApolloError(...ACCESS_DENIED)
+        } else {
+            try {
+                const decodedToken = jwt.verify(authorization, SECRET_KEY);
+                if (!decodedToken) return new ApolloError(...ACCESS_DENIED)
+                else {
+                    let authAdmin = await Admin.findById(decodedToken.user.id)
+                    if (authAdmin) {
+                        return authAdmin
+                    } else {
+                        return ApolloError(...ACCESS_DENIED)
+                    }
+                }
+            } catch (error) {
+                return new ApolloError(...ACCESS_DENIED)
             }
         }
     }
