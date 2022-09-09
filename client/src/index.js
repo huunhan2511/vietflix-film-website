e3453af761959577b9d9b5fd1f80f1581028ab36
HuuNhan <huunhan887@gmail.com>
2022-09-09 18:55:59 +0700

    Test deploy frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './app.scss';
 import App from './App';
 import {defaultTheme, Provider} from '@adobe/react-spectrum';
 import {
