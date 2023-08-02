3880c167019d148748c062437d69d1023d555976
toanak <khanhtoanverii@gmail.com>
2023-08-02 21:33:07 +0700

    fix

--- server/models/genre.js
+++ server/models/genre.js
@@ -1,9 +1,30 @@
 import mongoose from "mongoose";
+import Film from "./film.js";
+
+const GENRE_FOREIGN_KEY_EXCEPTION = 'Cannot delete genre. Films have a reference to this genre.';
+
 const genreSchema = new mongoose.Schema({
     name: {
         type: String,
     },
-}, { timestamps: {currentTime: () => (Date.now()+25200000)}, versionKey: false });
+}, { timestamps: { currentTime: () => (Date.now() + 25200000) }, versionKey: false });
+
+genreSchema.pre('findOneAndRemove', isSafeRemove);
+
+async function isSafeRemove(next) {
+    try {
+        const genreId = this.getQuery()._id;
+        const filmsWithGenre = await Film.find({ genres: genreId }).exec();
+
+        if (filmsWithGenre && filmsWithGenre.length > 0) {
+            return next(new Error(GENRE_FOREIGN_KEY_EXCEPTION));
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
 
 const Genre = mongoose.model('genre', genreSchema);
 export default Genre;
